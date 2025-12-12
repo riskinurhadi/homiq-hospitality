118b32e26569e05454829107bb5c593876691dc3
riskinurhadi <rizkibinmangtrisno@gmail.com>
2025-12-12 23:51:51 +0700

    modified:   dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -108,6 +108,9 @@ $koneksi->close();
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     
     <style>
+        html {
+            font-size: 90%;
+        }
         :root {
             --primary: #4361ee;
             --primary-soft: #eef2ff;
