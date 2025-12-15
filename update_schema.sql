bb4b022e8830fe63ab4e5741f840692481b56dfa
riskinurhadi <rizkibinmangtrisno@gmail.com>
2025-12-15 11:16:10 +0700

    modified:   update_schema.sql
    	modified:   detail_reservasi.php
    
    Untracked files:
    	proses_checkin.php

--- update_schema.sql
+++ update_schema.sql
@@ -2,3 +2,6 @@ ALTER TABLE tbl_reservasi
 CHANGE COLUMN tgl_checkin tgl_checkin DATETIME NOT NULL,
 CHANGE COLUMN tgl_checkout tgl_checkout DATETIME NOT NULL,
 ADD COLUMN jenis_booking VARCHAR(50) AFTER dibuat_oleh_user;
+
+ALTER TABLE tbl_reservasi
+ADD COLUMN path_identitas VARCHAR(255) DEFAULT NULL AFTER catatan_operator;
